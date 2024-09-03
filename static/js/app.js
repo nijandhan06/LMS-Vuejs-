d565cdc18af8f6cff4530edfb98cc1bf4df72044
nijandhan06 <159694302+nijandhan06@users.noreply.github.com>
2024-09-03 20:26:55 +0530

    Delete static/js directory

--- static/js/app.js
+++
@@ -1,8 +0,0 @@
-import router from "./routers.js";
-import navigationbar from "./components/navigationbar.js";
-import appfooter from "./components/appfooter.js";
-
-new Vue({
-  el: "#app",
-  router,
-});
