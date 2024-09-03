af11c422428a03a62e779061cd2a17f6499ea8b2
nijandhan06 <159694302+nijandhan06@users.noreply.github.com>
2024-09-03 20:30:31 +0530

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+import router from "./routers.js";
+import navigationbar from "./components/navigationbar.js";
+import appfooter from "./components/appfooter.js";
+
+new Vue({
+  el: "#app",
+  router,
+});
